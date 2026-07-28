3f0468999d0a8e06db989cdd0691d7c0978bb052
woojehong <hwj.rouget@gmail.com>
2026-07-28 17:20:34 +0900

    Fix showroom point marker preview

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'weight-v118-release-showroom-items';
+const CACHE = 'weight-v119-marker-preview';
 const ASSETS = [
   './index.html', './dashboard.html', './input.html', './import.html',
   './achievements.html', './compare.html', './dressroom.html', './shop.html', './guide.html', './visual-lab.html',
