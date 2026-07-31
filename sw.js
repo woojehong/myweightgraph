e9f95df0cc1f37d64588ecbbb04dfd61a0338ac9
woojehong <hwj.rouget@gmail.com>
2026-07-31 15:00:08 +0900

    Bust nutrition module cache on deploy

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'weight-v136-nutrition-beta';
+const CACHE = 'weight-v137-nutrition-beta-imports';
 const ASSETS = [
   './index.html', './dashboard.html', './input.html', './import.html',
   './achievements.html', './compare.html', './dressroom.html', './diet.html', './shop.html', './guide.html', './visual-lab.html',
